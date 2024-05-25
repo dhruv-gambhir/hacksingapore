c673a1ff5c243d500244a02184bf93d424742944
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2024-05-25 16:06:03 +0800

    Start page

--- frontend/dabao4you/src/App.js
+++ frontend/dabao4you/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import SignUp from './SignUp';
+import StartPage from './StartPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Dabao4You</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={'/'} element={<StartPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
