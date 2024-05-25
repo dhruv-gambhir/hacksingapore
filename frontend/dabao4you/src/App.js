6485dead1def711a1c4df13e887e69d483eba6ea
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2024-05-25 14:29:45 +0800

    Initial Frontend Structure

---
+++ frontend/dabao4you/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dabao4You</h1>
+    </div>
+  );
+}
+
+export default App;
