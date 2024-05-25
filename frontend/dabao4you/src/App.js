a087e4bc52259066ba56155e8d9a2634b618c252
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2024-05-26 04:32:50 +0800

    need_help

--- frontend/dabao4you/src/App.js
+++ frontend/dabao4you/src/App.js
@@ -3,6 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
 import StartPage from './StartPage';
+import Needy_Home from './need_help/MainPage';
+//import Needy_Profile from 'need_help/Profile';
+import Needy_Chat from './need_help/Chat';
+//import Volunteer_Home from 'give_help/MainPage';
+//import Volunteer_Profile from 'give_help/Profile';
+//import Volunteer_Chat from 'give_help/Chat';
+//import Volunteer_Tasks from 'give_help/Tasks';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 function App() {
   return (
@@ -10,6 +19,8 @@ function App() {
       <Routes>
         <Route path={'/'} element={<StartPage/>}/>
         <Route path={'/SignUp'} element={<SignUp/>}/>
+        <Route path={'/Home'} element={<Needy_Home/>}/>
+        <Route path={'/Chat'} element={<Needy_Chat/>}/>
       </Routes>
     </Router>
   );
