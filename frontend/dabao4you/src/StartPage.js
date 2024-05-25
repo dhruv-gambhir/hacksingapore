b5ee79aa7dbe027aa850599ac325b699be2e7bb6
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2024-05-25 16:41:13 +0800

    Start Page updates

--- frontend/dabao4you/src/StartPage.js
+++ frontend/dabao4you/src/StartPage.js
@@ -1,15 +1,22 @@
+import './App.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function StartPage() {
+    const handleButtonClick = () => {
+        console.log('Button pressed');
+    };
 
     return(
-    <div class='container-fluid text-black text-center p-5' style={{ backgroundColor: '#C5CAF2' }}>
-        <p class="h1">Dabao4You!</p>
-        <img src="/8635980.jpg" alt="Image" className="img-fluid mt-3" />
-        <p class="h3">Spread joy as you go</p>
-        <button type="button" class="btn btn-light" style={{ fontColor: 'white' }}>Retrieve Myinfo with Singpass</button>
-
+        <div className="container-fluid text-center bg-custom-purple min-vh-100">
+        <div className="p-3">
+            <p className="h1 text-white mt-3">Dabao4You!</p>
+            <img src="/8635980.jpg" alt="Image" className="img-fluid mt-4" />
+            <p className="h3 text-white">Spread joy as you go</p>
+            <button type="button" className="btn btn-light mt-3" onClick={handleButtonClick}>
+                Retrieve Myinfo with Singpass
+            </button>
+        </div>
     </div>
     )
 }
